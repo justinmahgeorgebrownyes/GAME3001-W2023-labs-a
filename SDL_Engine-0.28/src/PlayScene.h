6979bfe4f16b461c26a41be81f60a138bf6caa3f
Justin Mah <91901581+justinmahgeorgebrownyes@users.noreply.github.com>
2023-01-10 11:38:44 -0500

    hello

---
+++ SDL_Engine-0.28/src/PlayScene.h
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef __PLAY_SCENE__
+#define __PLAY_SCENE__
+
+#include "Scene.h"
+#include "Plane.h"
+#include "Player.h"
+#include "Button.h"
+#include "Label.h"
+
+class PlayScene : public Scene
+{
+public:
+	PlayScene();
+	~PlayScene() override;
+
+	// Scene LifeCycle Functions
+	virtual void Draw() override;
+	virtual void Update() override;
+	virtual void Clean() override;
+	virtual void HandleEvents() override;
+	virtual void Start() override;
+private:
+	// IMGUI Function
+	void GUI_Function() const;
+	std::string m_guiTitle;
+	
+	glm::vec2 m_mousePosition;
+
+	Plane* m_pPlaneSprite;
+	Player* m_pPlayer;
+	bool m_playerFacingRight;
+
+	// UI Items
+	Button* m_pBackButton;
+	Button* m_pNextButton;
+	Label* m_pInstructionsLabel;
+};
+
+#endif /* defined (__PLAY_SCENE__) */
